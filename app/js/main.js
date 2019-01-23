6987fa08927015634fb332b4e573363ea35560c4
Pavel_Belove <agink@inbox.ru>
2019-01-23 07:07:55 +0300

    copy progect

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+function message(text) {
+    console.log('Вызван метод message; text = ', text);
+}
+
+message('Первый вызов message');
+message('Второй вызов message');
+message('Третий вызов message');
